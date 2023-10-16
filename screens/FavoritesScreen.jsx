f91e8a7d70cfb68c3891980539d020243d667f20
Radovan Kotarac <kotaracradovan@gmail.com>
2023-10-16 18:36:26 +0200

    commit

---
+++ screens/FavoritesScreen.jsx
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import MealsList from "../components/MealsList/MealsList";
+import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
+import { StyleSheet, View, Text, } from "react-native";
+
+function FavoritesScreen() {
+  const favoriteMealsCtx = useContext(FavoritesContext);
+
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id)
+  );
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootCotainer}>
+        <Text style={styles.text}>No Favorite Meals Yet.</Text>
+      </View>
+    );
+  }
+
+  return <MealsList items={favoriteMeals} />;
+}
+
+export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootCotainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
---
+++ store/context/favorites-context.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from "react";
+
+export const FavoritesContext = createContext({
+  ids: [],
+  addFavorite: (id) => {},
+  removeFavorite: (id) => {},
+});
+
+function FavoritesContextProvider({ children }) {
+  const [favoriteMealIds, setFavoriteMealId] = useState([]);
+
+  function addFavorite(id) {
+    setFavoriteMealId((currentFavIds) => [...currentFavIds, id]);
+  }
+
+  function removeFavorite(id) {
+    setFavoriteMealId((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  }
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite, 
+  };
+
+
+
+  return <FavoritesContext.Provider value={value}>{children}</FavoritesContext.Provider>;
+}
+export default FavoritesContextProvider;
